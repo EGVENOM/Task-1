9f65b0c0f9523c9183b988134c75f60189d68744
EGVENOM <93351045+EGVENOM@users.noreply.github.com>
2021-11-28 22:14:05 +0200

    Task 1

---
+++ Task 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int first_number=8;
+float second_number=3.5;
+char character='A';
+char word[20]="Hello,C Language";
+printf("%d is an integer number\n",first_number);
+printf("%f is a number containg decimal point\n" ,second_number);
+printf("%c is a character\n",character);
+printf("%s",word);
+
+}
+
